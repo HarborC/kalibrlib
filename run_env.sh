43ce56f70faad22959a22a9133efcbe2696bc2cb
HarborC <1391586089@qq.com>
2025-06-30 13:30:56 +0800

    update

--- run_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-BASE_DIR=$(cd $(dirname $0);pwd)
-# echo "BASE_DIR: ${BASE_DIR}"
-echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:${BASE_DIR}/install/lib"
-echo "export PYTHONPATH=\$PYTHONPATH:${BASE_DIR}/install/lib"
